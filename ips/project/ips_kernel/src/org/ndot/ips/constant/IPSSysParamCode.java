3da5f62e851a17f77764029f019adcebad787c7c
ipsman2009 <ipsman2009@8821288c-791b-11de-91f8-ab615253d9e8>
2009-08-18 09:33:07 +0000

    将获取 macbuffer 方法放到 IPSReport中，方便调用

--- ips/project/ips_kernel/src/org/ndot/ips/constant/IPSSysParamCode.java
+++ ips/project/ips_kernel/src/org/ndot/ips/constant/IPSSysParamCode.java
@@ -4,4 +4,5 @@ public class IPSSysParamCode {
 	public static String IPS0001 = "0001";// 核心清算日期
 	public static String IPS0002 = "0002";// 卡表版本号
 	public static String IPS0003 = "0003";// 控制参数版本号
+	public static String IPS0004 = "0004";// BF128
 }
